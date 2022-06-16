7b4317b1f54705d0225ae8ab8428d3b22e54c270
Lalit Kumar <lalit.k1997@gmail.com>
2022-06-16 21:22:19 +0530

    Navbar-Footer-Search-Data
    Navbar-Footer-Search-Data

---
+++ Components/navbar.js
@@ -0,0 +1,52 @@
+function navbar() {
+  return `<div id="navbar">
+    <div id="navbar-left">
+      <div class="links">
+        <a href="#">MENSWEAR</a>
+        <a href="#">WOMENSWEAR</a>
+        <a href="#">EVERYTHING ELSE</a>
+        <!-- <a href="/">SEARCH</a> -->
+        <div class="dropdown">
+          <button class="dropbtn">SEARCH</button>
+          <div class="dropdown-content">
+            <div>
+              <a href="#">MENSWEAR</a>
+              <a href="#">WOMENSWEAR</a>
+              <a href="#">EVERYTHING ELSE</a>
+            </div>
+            <input
+              type="text"
+              name=""
+              id="search_query"
+              placeholder="SEARCH"
+            />
+            <div id="search_btn">
+              <img src="../images/search.svg" alt="" />
+            </div>
+            <input type="checkbox" name="" id="" />
+            <span>SALES ONLY</span>
+            <!-- <button>CLOSE</button> -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="icon">
+      <img
+        src="https://res.cloudinary.com/ssenseweb/image/upload/v1471963917/web/ssense_logo_v2.svg"
+        alt="iconn ssense"
+      />
+    </div>
+    <div id="navbar-right">
+      <div class="links2">
+        <a href="#">ENGLISH</a>
+        <a href="#" id="user_name">LOGIN</a>
+        <a href="#">WISHLIST</a>
+        <a href="#"
+          >SHOPPING BAG (<div id="item_count"><!--append cart-items-count here--></div>)</a
+        >
+      </div>
+    </div>
+  </div>`;
+}
+
+export { navbar };
