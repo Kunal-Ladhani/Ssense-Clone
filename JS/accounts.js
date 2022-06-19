4bbd693fe730ffb0b840231c3c61b9c3c4301d3e
Lalit Kumar <lalit.k1997@gmail.com>
2022-06-19 15:29:54 +0530

    item-count-login-logout

--- JS/accounts.js
+++ JS/accounts.js
@@ -10,3 +10,16 @@ footdiv.innerHTML = footer();
 
 let container = document.getElementById("scontainer");
 container.innerHTML = account();
+
+document.querySelector("#logout").addEventListener("click", logoutUser);
+
+function logoutUser(){
+    // event.preventDefault();
+    // console.log("Hello");
+    let thisUser = JSON.parse(localStorage.getItem("thisUser")) || {};
+    let ssenseUser = JSON.parse(localStorage.getItem("ssenseUser")) || {};
+    thisUser = {};
+    ssenseUser = {};
+    localStorage.setItem("thisUser", JSON.stringify(thisUser));
+    localStorage.setItem("ssenseUser", JSON.stringify(ssenseUser));
+}
--- JS/navbar.js
+++ JS/navbar.js
@@ -1,36 +1,72 @@
-// TO SEARCH ITEMS ITEMS : 
+// TO SEARCH ITEMS ITEMS :
 document.querySelector("#search_btn").addEventListener("click", searchQuery);
 
 // SEARCH BAR
-function searchQuery(){
-    console.log("searchQuery Func Called !");
-    let query = document.querySelector("#search_query").value;
-    if(query === "shorts"){
-        // REDIRECT TO ITEM PAGE OF SHORTS
-    }else if(query === "tshirts"){
-        // REDIRECT TO ITEM PAGE OF SHORTS
-    }else{
-        alert("NOT AVAILABLE!");
-    }
+function searchQuery() {
+  console.log("searchQuery Func Called !");
+  let query = document.querySelector("#search_query").value;
+  if (query === "menswear") {
+    // REDIRECT TO ITEM PAGE OF SHORTS
+    window.location.href = "../HTML/mens.html";
+  } else if (query === "womenswear") {
+    // REDIRECT TO ITEM PAGE OF SHORTS
+    window.location.href = "../HTML/womens.html";
+  } else {
+    alert("NOT AVAILABLE!");
+  }
 }
 
 // TO UPDATE CART ITEMS :
-function updateCartcount(){
-    document.querySelector("#item_count").innerText = '0';
-    // IF NO PRODUCTS ADDED TO CART => 0
-    // ELSE COUNT ELE AND UPDATE
+function updateCartcount() {
+  let countcartItems = JSON.parse(localStorage.getItem("cart-data"));
+  // console.log(countcartItems);
+  if(countcartItems === undefined){
+    document.querySelector("#item_count").innerText = "0";
+  }else{
+    document.querySelector("#item_count").innerText = countcartItems.length;
+  }
+  
+  // IF NO PRODUCTS ADDED TO CART => 0
+  // ELSE COUNT ELE AND UPDATE
 }
 
-// TO UPDATE USERNAME : 
-function updateUsername(){  
-    document.querySelector("#user_name").innerText = 'LOGIN';
-    // IF USER EXISTS IN LS => SHOW USERNAME
-    // IF USER DOES NOT EXISTS IN LS => SHOW LOGIN
+// TO UPDATE USERNAME :
+function updateUsername() {
+  let username = JSON.parse(localStorage.getItem("thisUser")) || {};
+  // console.log(username.UserName);
+  // IF USER EXISTS IN LS => SHOW USERNAME
+  // IF USER DOES NOT EXISTS IN LS => SHOW LOGIN
+  if (username.UserName === undefined && username.password == undefined) {
+    document.querySelector("#user_name").innerText = "LOGIN";
+  } else if (
+    username.UserName !== undefined &&
+    username.password !== undefined
+  ) {
+    document.querySelector("#user_name").innerText = "ACCOUNT";
+  }
 }
 
-document.querySelector("#company-icon-nav").addEventListener("click", toLandingPage);
-function toLandingPage(){
-    window.location.href = "../index.html";
+
+
+// IF ALREADY LOGGED-IN GO TO ACCOUNTS 
+// THEN LOGIUT
+document.querySelector("#user_name").addEventListener("click", toAccountPage);
+
+function toAccountPage() {
+    event.preventDefault();
+    console.log("Hello");
+    if(document.querySelector("#user_name").innerText === "ACCOUNT"){
+        window.location.href = "../HTML/accounts.html";
+    }else{
+        window.location.href = "../HTML/login.html";
+    }
+}
+
+document
+  .querySelector("#company-icon-nav")
+  .addEventListener("click", toLandingPage);
+function toLandingPage() {
+  window.location.href = "../index.html";
 }
 
 updateUsername();
