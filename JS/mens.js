49236b523630e553b714f89c60bf15e5d4c14fbb
SaurabhPagrut <saurabhpagrut2016@gmail.com>
2022-06-19 11:18:18 +0530

    imported navbar and footer in mens and womens page

--- JS/mens.js
+++ JS/mens.js
@@ -1,3 +1,12 @@
+import {navbar} from "../Components/navbar.js";
+import {footer} from "../Components/footer.js";
+
+let navdiv = document.getElementById("navbar");
+navdiv.innerHTML = navbar();
+
+let footdiv = document.getElementById("footer");
+footdiv.innerHTML = footer();
+
 let category
 
 let categories = document.querySelectorAll(".elements>a")
